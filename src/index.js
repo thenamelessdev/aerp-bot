607952a2dfdd583e4b9b39f04ee28875bbf54024
thenamelessdev <aron.minarovits@gmail.com>
2025-09-06 16:28:08 +0200

    started making ping command

--- src/index.js
+++ src/index.js
@@ -5,12 +5,25 @@ const { token } = require('../config.json');
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildModeration, GatewayIntentBits.MessageContent] });
 
+const commands = [
+	new SlashCommandBuilder().setName("ping").setDescription("A ping command"),
+]
+
 // When the client is ready, run this code (only once).
 // The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
 // It makes some properties non-nullable.
 client.once(Events.ClientReady, readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
+
+// ping command reply
+client.on(Events.InteractionCreate, async (interaction) => {
+	if (interaction.commandName == "ping") {
+		interaction.reply("Pong!")
+		console.log("Ping command used.")
+	}
+})
+
 
 // Log in to Discord with your client's token
 client.login(token);
